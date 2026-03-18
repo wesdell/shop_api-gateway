d9da7210b6b031552f742ae496dfc994c5deb76d
Sebastian Guerrero <wesdell17@gmail.com>
2026-03-18 15:18:17 -0500

    Setup CORS configuration

--- src/main/java/com/wesdell/shop/gateway/config/SecurityConfig.java
+++ src/main/java/com/wesdell/shop/gateway/config/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -21,6 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+                .cors(c -> c.configurationSource(configureCORS()))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(freeResourcesURLs)
                         .permitAll()
@@ -31,6 +37,19 @@ public class SecurityConfig {
                         .jwt(Customizer.withDefaults())
                 )
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource configureCORS() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
 }
